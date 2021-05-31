15bbba8ac5e84abd5471bc08dff75ae54f673931
ObjectOrientedLife <smsychjy96@gmail.com>
2021-06-01 04:24:35 +0900

    working on sample graph

--- include/backtrack.h
+++ include/backtrack.h
@@ -13,27 +13,35 @@
 #include <queue>
 #include <vector>
 #include <set>
+using namespace std;
 
 class Backtrack {
  public:
-  Backtrack();
-  ~Backtrack();
+    Backtrack();
+    ~Backtrack();
 
-  void PrintAllMatches(const Graph &data, const Graph &query,
-                       const CandidateSet &cs);
+   void PrintAllMatches(const Graph &data, const Graph &query,
+                     const CandidateSet &cs);
 
-  void BuildDAG(const Graph &query, const CandidateSet &cs);
-
-  void Track(const Graph &query, const CandidateSet &cs);
-
-  void GetNext(const CandidateSet &cs, size_t curr);
-
-  void Debug(const Graph &data, const Graph &query,
-             const CandidateSet &cs);
+   void BuildDAG(const Graph &query, const CandidateSet &cs, Vertex &root);
+   void Track(const Graph &data, const CandidateSet &cs, vector<pair<Vertex, Vertex>> M, const Vertex root);
+   void UpdateExtendable(const Graph &data, const CandidateSet &cs, Vertex added, Vertex candidate);
+   void Debug(const Graph &data, const Graph &query,
+            const CandidateSet &cs);
 
  private:
-    std::vector<std::vector<Vertex>> adj_list;
-    bool check_vertex[10001];
+   vector<vector<Vertex>> adj_list;
+   bool check_vertex[10001];
+
+   vector<int32_t> parentCount; // Stores the number of parents of each vertex
+   vector<int32_t> matchedParentCount; // Tracks the number of parents matched
+
+   vector<vector<int32_t>> candidateMatchedParentCount;
+   vector<vector<bool>> availableCandidates;
+   vector<int32_t> cmuCount;
+
+   set<Vertex> extendable;
+   vector<bool> visited;
 };
 
 #endif  // BACKTRACK_H_
